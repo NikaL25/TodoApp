597fc4a01ff6c55d7b8da0b6eaa5f53495122bfb
NikaL25 <mr.lomiashvil@mail.ru>
2024-10-11 21:28:58 +0400

    todo App

---
+++ Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace ToDoApp.Models
+{
+    public class Category
+    {
+        public string CategoryId { get; set; } = String.Empty;
+        public string Name { get; set; } = String.Empty;
+    }
+}
